2617264ef5c8aeed008e37da54662aeac6973dde
mjstackhouse <micahjstackhouse@gmail.com>
2023-07-03 16:27:46 -0600

    feat: Added Neuro-Aid brain icon for the manifest.json to display in the browser. Also got rid of 2 unnecessary console.logs in the Go-Back-To-Top component, and changed the form label's 'for' attribute to the correct 'htmlFor' in the MailingList compponent.

--- client/src/components/MailingList.js
+++ client/src/components/MailingList.js
@@ -30,7 +30,7 @@ function MailingList() {
   return (
     <div className="flex basis-full h-36 self-end bg-white min-h-[120px] mb-4" id="mailing-list-container">
       <form className="flex flex-wrap mx-auto place-items-center justify-center py-6 px-4" onSubmit={handleSubmit}>
-          <label for="email-input" className="basis-full font-semibold text-gray text-base mb-2 sm:text-lg">Join our mailing list for news and updates about Neuro-Aid</label>
+          <label htmlFor="email-input" className="basis-full font-semibold text-gray text-base mb-2 sm:text-lg">Join our mailing list for news and updates about Neuro-Aid</label>
           <input id="email-input" className="h-[2rem] w-[175px] sm:w-[300px] ml-2 rounded-lg border-2 border-solid border-gray hover:border-orange" type="email" maxLength='254' onChange={(e) => setEmail(e.target.value)} required></input>
           <button type="submit" className="bg-black hover:bg-gray font-semibold text-white text-base sm:text-lg rounded-3xl py-2 px-4 ml-2 shadow-md" id='subscribe-button'>Subscribe</button>
       </form>
